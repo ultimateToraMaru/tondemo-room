3f0238c2c8f48c7b06f8b1cb074d206082c4cc44
ultimateToraMaru <60610126+ultimateToraMaru@users.noreply.github.com>
2024-12-28 16:17:04 +0900

    マジカルポン漫画を表示するように修正

---
+++ magical-pon-test-puzzle/board.py
@@ -0,0 +1,6 @@
+class Boad:
+    """
+    This class represents the board of the game. It is a 2D array of cells.
+    """
+    def __init__(self):
+        pass
---
+++ magical-pon-test-puzzle/candy.py
@@ -0,0 +1,6 @@
+class Candy:
+    """
+    This class represents a candy object.
+    """
+    def __init__(self):
+        pass
